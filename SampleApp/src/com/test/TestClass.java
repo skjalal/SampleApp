63d4d46fc1a063b01c207dabc423df213eeae878
jalaluddins <jalaluddins@ibi158.infobrain.com>
2017-09-19 19:21:15 +0530

    after adding Logger

--- SampleApp/src/com/test/TestClass.java
+++ SampleApp/src/com/test/TestClass.java
@@ -1,10 +1,14 @@
 package com.test;
 
+import java.util.logging.Logger;
+
 public class TestClass {
+	private static final Logger log = Logger.getLogger("TestClass");
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
-	}
+		log.info("Enter into Main Method...!");
 
+		log.info("Enter into Main Method...!");
+		return;
+	}
 }
